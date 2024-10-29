15cbe944b7e392a31ac5882b4079f562505c0689
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-10-29 10:02:39 +0800

    [Nicolo] added base.html, styling, integrated base to index

--- app/foodguard/foodguard/settings.py
+++ app/foodguard/foodguard/settings.py
@@ -125,7 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Adjust this path according to your project structure
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
