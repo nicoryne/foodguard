840736ff3495fb02f409f3b209b16f7c4bf84398
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-11-05 12:42:22 +0800

    [Nicolo] crud shopping list

--- app/foodguard/apps/shoppinglist/urls.py
+++ app/foodguard/apps/shoppinglist/urls.py
@@ -1,4 +1,3 @@
-# shopping_list/urls.py
 from django.urls import path
 from .views import (
     ShoppingListView,
@@ -7,13 +6,17 @@ from .views import (
     IngredientsToBuyListView,
     IngredientsInCartListView,
     IngredientInCartDeleteView,
+    IngredientUpdateView,
+    IngredientDeleteView,
 )
 
 urlpatterns = [
     path('', ShoppingListView.as_view(), name='shopping-list'),
     path('<int:pk>/', ShoppingListDetailView.as_view(), name='shopping-list-detail'),
-    path('new/', ShoppingListIngredientCreateView.as_view(), name='shopping-list-ingredient-create'),
+    path('<int:shopping_list_id>/new/', ShoppingListIngredientCreateView.as_view(), name='shopping-list-ingredient-create'),
     path('<int:shopping_list_id>/ingredients/', IngredientsToBuyListView.as_view(), name='ingredients-to-buy-list'),
     path('<int:shopping_list_id>/cart/', IngredientsInCartListView.as_view(), name='ingredients-in-cart-list'),
     path('cart/<int:pk>/delete/', IngredientInCartDeleteView.as_view(), name='ingredient-in-cart-delete'),
+    path('<int:shopping_list_id>/ingredient/<int:pk>/update/', IngredientUpdateView.as_view(), name='ingredient-update'),
+    path('<int:shopping_list_id>/ingredient/<int:pk>/delete/', IngredientDeleteView.as_view(), name='ingredient-delete'),  # Updated paths
 ]
