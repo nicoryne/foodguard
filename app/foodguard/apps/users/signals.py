f2c687ea25ec9cc1b6173d459feed20982e4aa9d
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-11-05 09:26:13 +0800

    [Nicolo] working on shopping list template

--- app/foodguard/apps/users/signals.py
+++ app/foodguard/apps/users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from .models import User
+from django.conf import settings
+from ..shoppinglist.models import ShoppingList
 
 @receiver(post_save, sender=User)
 def log_user_creation(sender, instance, created, **kwargs):
@@ -8,7 +10,13 @@ def log_user_creation(sender, instance, created, **kwargs):
         print(f"New user created: {instance.fname} {instance.lname} (Email: {instance.email})")
     else:
         print(f"User updated: {instance.fname} {instance.lname} (Email: {instance.email})")
+        
+@receiver(post_save, sender=User)
+def create_shopping_list(sender, instance, created, **kwargs):
+    if created:
+        ShoppingList.objects.create(user=instance, capacity=10)
 
 @receiver(pre_delete, sender=User)
 def log_user_deletion(sender, instance, **kwargs):
     print(f"User deleted: {instance.fname} {instance.lname} (Email: {instance.email})")
+
