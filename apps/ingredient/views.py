d8e1947a32ce6bdecb9ec5161a5f01a0a1f8c605
Gabe <sandiegogabejeremy@gmail.com>
2024-11-21 03:57:57 +0800

    [Gabe] Ingredients 80% - UI, Urgent Consumption Logic, Filtering

--- apps/ingredient/views.py
+++ apps/ingredient/views.py
@@ -1,12 +1,29 @@
 from .models import Ingredient
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from datetime import timedelta
+from django.utils.timezone import now
 
 # Ingredient List View
 class IngredientListView(ListView):
     model = Ingredient
     template_name = 'ingredient_list.html'
     context_object_name = 'ingredients'
+
+    def get_context_data(self, **kwargs):
+        # Call the parent class method to get the default context
+        context = super().get_context_data(**kwargs)
+        
+        # Filter urgent ingredients (expiry_date is within 4 days from date_purchased)
+        urgent_ingredients = Ingredient.objects.filter(
+            date_purchased__isnull=False,
+            expiry_date__isnull=False,
+            expiry_date__lte=(now().date() + timedelta(days=4))
+        )
+        
+        # Add urgent ingredients to the context
+        context['urgent_ingredients'] = urgent_ingredients
+        return context
 
 # Ingredient Detail View
 class IngredientDetailView(DetailView):
