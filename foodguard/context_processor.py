45ff9027e8880821118eb6cec7a133b2a1a86d88
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-12-10 08:12:06 +0800

    max 5 notifications, more tts details on shoppinglist

--- foodguard/context_processor.py
+++ foodguard/context_processor.py
@@ -2,7 +2,7 @@ from apps.notifications.models import Notification
 
 def notifications(request):
     if request.user.is_authenticated:
-        user_notifications = Notification.objects.filter(user=request.user).order_by('-created_at')
+        user_notifications = Notification.objects.filter(user=request.user).order_by('-created_at')[:5]
     else:
         user_notifications = []
     return {'notifications': user_notifications}
